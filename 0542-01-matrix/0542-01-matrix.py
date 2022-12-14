49aa5d93cde8a71e81befd7fdc3dc7003c340d27
John Xie <65366906+j0hnxie@users.noreply.github.com>
2022-12-13 20:40:02 -0500

    Time: 595 ms (85.90%), Space: 17.1 MB (27.13%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -19,25 +19,21 @@ class Solution(object):
             
         
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        dist = 0
         # print(str(i) + " " + str(j))
         while bfs:
             # print(bfs)
-            size = len(bfs)
-            for k in range(size):
-                cur = bfs.popleft()
-                visited.add((cur[0], cur[1]))
-                mat[cur[0]][cur[1]] = min(dist, mat[cur[0]][cur[1]])
-                for direction in directions:
-                    newY = cur[0] + direction[0]
-                    newX = cur[1] + direction[1]
-                    if (newY, newX) not in visited:
-                        if newY < 0 or newY >= len(mat):
-                            continue
-                        elif newX < 0 or newX >= len(mat[0]):
-                            continue
-                        bfs.append((newY, newX))
-            dist += 1
+            cur = bfs.popleft()
+            for direction in directions:
+                newY = cur[0] + direction[0]
+                newX = cur[1] + direction[1]
+                if (newY, newX) not in visited:
+                    if newY < 0 or newY >= len(mat):
+                        continue
+                    elif newX < 0 or newX >= len(mat[0]):
+                        continue
+                    bfs.append((newY, newX))
+                    visited.add((newY, newX))
+                    mat[newY][newX] = mat[cur[0]][cur[1]] + 1
                 
         return mat
                 
