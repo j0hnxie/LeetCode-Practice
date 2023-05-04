80cc22a3796c1ecaf981a423d6c4e98c69c49952
John Xie <johnjxie@gmail.com>
2023-05-04 18:09:49 -0400

    Time: 19 ms (85.69%), Space: 13.3 MB (98.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -11,25 +11,48 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        head = ListNode()
-            
-        current = head
-            
-        while list1 and list2:
-            # print(head)
-            if list1.val < list2.val:
-                current.next = list1
-                current = current.next
-                list1 = list1.next
-            else:
-                current.next = list2
-                current = current.next
-                list2 = list2.next
         
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-                
-        return head.next
-                
+        if list1 == None:
+            return list2
+        if list2 == None:
+            return list1
+        
+        # head = list1
+        # cur = head
+        # cur1 = list1.next
+        # cur2 = list2
+        if list1.val < list2.val:
+            head = list1
+            cur = head
+            cur1 = list1.next
+            cur2 = list2
+        else:
+            head = list2
+            cur = head
+            cur1 = list1
+            cur2 = list2.next
+        
+#         print(cur1.val)
+#         print(cur2.val)
+            
+        while cur1 != None and cur2 != None:
+            if (cur1.val < cur2.val):
+                cur.next = cur1
+                cur1 = cur1.next
+            else:
+                cur.next = cur2
+                cur2 = cur2.next
+            # print(cur.val)
+            cur = cur.next
+        
+        if cur1 != None:
+            cur.next = cur1
+        else:
+            cur.next = cur2
+        
+        return head
+            
+
+            
+        
+            
