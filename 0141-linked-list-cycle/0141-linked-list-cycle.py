0a2b55913b8ddcd22383fbcbb61185617484abb0
John Xie <johnjxie@gmail.com>
2023-05-07 13:28:55 -0400

    Time: 57 ms (9.99%), Space: 20.4 MB (84.00%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -14,19 +14,16 @@ class Solution(object):
         if head == None or head.next == None:
             return False
     
-        pointer1 = head
-        pointer2 = head.next.next
+        fast = head.next
+        slow = head
         
-        while pointer1 != pointer2:
-            if pointer1 != None:
-                pointer1 = pointer1.next
-            if pointer2 != None:
-                pointer2 = pointer2.next
-            if pointer2 != None:
-                pointer2 = pointer2.next
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
             
-            
-        if pointer1 == None or pointer2 == None:
-            return False
-        else:
-            return True
+            if slow == fast:
+                return True
+        
+        return False
+                
+        
