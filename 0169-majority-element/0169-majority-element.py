9d810470dcf95e2c510aa096d181b5531a27cf99
John Xie <johnjxie@gmail.com>
2023-05-09 12:20:06 -0400

    Time: 141 ms (23.12%), Space: 15 MB (57.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -11,9 +11,10 @@ class Solution(object):
         n /= 2
         for i in nums:
             # print(i)
+            # counts[i] += 1
             cur = counts.get(i, 0)
             counts[i] = cur + 1
-            if cur + 1 > n:
+            if cur >= n:
                 return i
             
         
