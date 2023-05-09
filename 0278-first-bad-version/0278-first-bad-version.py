7d26f3a58342c05f2ca1fd04871f4fd959051d43
John Xie <johnjxie@gmail.com>
2023-05-08 20:35:35 -0400

    Time: 23 ms (11.12%), Space: 13.5 MB (10.09%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -9,21 +9,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
         l = 1
-        r = n
-        while l <= r:
-            mid = (l + r) // 2
-            midTF = isBadVersion(mid)
-            if midTF:
-                if isBadVersion(mid - 1):
+        r = n 
+        while l < r:
+            mid = l + ((r - l) / 2)
+            if isBadVersion(mid) and not isBadVersion(mid - 1):
+                return mid
+            else:
+                if isBadVersion(mid):
                     r = mid - 1
                 else:
-                    return mid
-            else:
-                if not isBadVersion(mid + 1):
                     l = mid + 1
-                else:
-                    return mid + 1
-                
-        return -1
+        return l
