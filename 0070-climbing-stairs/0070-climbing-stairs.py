af4a8a4ef74509580d30882cc1b2f0455d3c79bc
John Xie <johnjxie@gmail.com>
2023-05-08 21:00:39 -0400

    Time: 8 ms (96.03%), Space: 13.4 MB (63.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -6,11 +6,10 @@ class Solution(object):
         """
         
         dp = []
-        dp.append(0)
         dp.append(1)
-        dp.append(2)
+        dp.append(1)
         
-        for i in range(3, n + 1):
+        for i in range(2, n + 1):
             dp.append(dp[i - 1] + dp[i - 2])
         
         return dp[n]
