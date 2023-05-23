07ab74202e53a061fb6b07b5e823e6066a5eab55
John Xie <johnjxie@gmail.com>
2023-05-23 14:20:25 -0400

    Time: 466 ms (86.96%), Space: 15.7 MB (97.83%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -9,9 +9,7 @@ class Solution(object):
         
         for i in range(len(mat)):
             for j in range(len(mat[i])):
-                if mat[i][j] == 0:
-                    mat[i][j] = 0
-                else:
+                if mat[i][j] != 0:
                     mat[i][j] = float('inf')
                     if i > 0:
                         mat[i][j] = min(mat[i][j], mat[i - 1][j] + 1)
@@ -20,9 +18,7 @@ class Solution(object):
         
         for i in range(len(mat) - 1, -1, -1):
             for j in range(len(mat[i]) - 1, -1, -1):
-                if mat[i][j] == 0:
-                    mat[i][j] = 0
-                else:
+                if mat[i][j] != 0:
                     if i < len(mat) - 1:
                         mat[i][j] = min(mat[i][j], mat[i + 1][j] + 1)
                     if j < len(mat[i]) - 1:
