43414c51915181b32158f4c2f7d3e96e525ccf42
John Xie <johnjxie@gmail.com>
2023-05-23 14:20:00 -0400

    Time: 477 ms (83.26%), Space: 15.8 MB (97.83%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -5,26 +5,27 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        result = [[float('inf')] * len(mat[0]) for i in range(len(mat))]
+        # result = [[float('inf')] * len(mat[0]) for i in range(len(mat))]
         
         for i in range(len(mat)):
             for j in range(len(mat[i])):
                 if mat[i][j] == 0:
-                    result[i][j] = 0
+                    mat[i][j] = 0
                 else:
+                    mat[i][j] = float('inf')
                     if i > 0:
-                        result[i][j] = min(result[i][j], result[i - 1][j] + 1)
+                        mat[i][j] = min(mat[i][j], mat[i - 1][j] + 1)
                     if j > 0:
-                        result[i][j] = min(result[i][j], result[i][j - 1] + 1)
+                        mat[i][j] = min(mat[i][j], mat[i][j - 1] + 1)
         
         for i in range(len(mat) - 1, -1, -1):
             for j in range(len(mat[i]) - 1, -1, -1):
                 if mat[i][j] == 0:
-                    result[i][j] = 0
+                    mat[i][j] = 0
                 else:
                     if i < len(mat) - 1:
-                        result[i][j] = min(result[i][j], result[i + 1][j] + 1)
+                        mat[i][j] = min(mat[i][j], mat[i + 1][j] + 1)
                     if j < len(mat[i]) - 1:
-                        result[i][j] = min(result[i][j], result[i][j + 1] + 1)
+                        mat[i][j] = min(mat[i][j], mat[i][j + 1] + 1)
         
-        return result
+        return mat
