62381882e4c1e31774dfaee574185c272c79e9d7
John Xie <johnjxie@gmail.com>
2023-06-12 17:00:32 -0400

    Time: 664 ms (69.18%), Space: 19.4 MB (84.05%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -6,19 +6,5 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        test = []
-        result = []
-        for i in range(len(points)):
-            xDist = points[i][0] * points[i][0]
-            yDist = points[i][1] * points[i][1]
-            totalDist = xDist + yDist
-            res = -1 * sqrt(totalDist)
-            if len(test) < k:
-                heapq.heappush(test, [res, i])
-            else: 
-                heapq.heappushpop(test, [res, i])
-            
-        for i in test:
-            result.append(points[i[1]])
-            
-        return result
+        res = sorted(points, key=lambda x: x[0] * x[0] + x[1] * x[1])
+        return res[:k]
