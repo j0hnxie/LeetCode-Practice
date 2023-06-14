125811fcc3893dbf268f500569812c4333c07ea9
John Xie <johnjxie@gmail.com>
2023-06-14 00:14:39 -0400

    Time: 52 ms (47.28%), Space: 14.2 MB (34.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,22 +5,26 @@ class Solution(object):
         :rtype: int
         """
         
-        curMax = 0
-        exists = set()
-        begin = 0
-        end = 0
-        
-        while end < len(s):
-            # print(str(begin) + " " + str(end))
-            if s[end] not in exists:
-                exists.add(s[end])
-                end += 1
-                curMax = max(curMax, end - begin)
+        counts = {}
+        length = 0
+        maxLength = 0
+        start = 0
+        for end in range(len(s)):
+            newCount = counts.get(s[end], 0) + 1
+            counts[s[end]] = newCount
+            if counts[s[end]] > 1:
+                while s[start] != s[end]:
+                    counts[s[start]] -= 1
+                    start += 1
+                counts[s[start]] -= 1
+                start += 1
+                length = end - start + 1
+                # print(start)
+                # print(end)
+                # print(length)
             else:
-                while s[end] != s[begin]:
-                    exists.remove(s[begin])
-                    begin += 1
-                begin += 1
-                end += 1
-                
-        return curMax
+                length += 1
+            # print(counts)
+            maxLength = max(maxLength, length)
+            
+        return maxLength
