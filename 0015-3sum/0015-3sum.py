b6e6b5dd1eb0a6a3aa88c208d9eea5159f2cbdec
John Xie <johnjxie@gmail.com>
2023-06-14 22:16:35 -0400

    Time: 8785 ms (5.00%), Space: 17 MB (27.54%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        # nums.sort()
+        result = set()
+        for i in range(len(nums)):
+            target = nums[i] * -1
+            complements = {}
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                comp = target - nums[j]
+                if comp in complements:
+                    insertion = [nums[i], nums[j], nums[complements[comp]]]
+                    insertion.sort()
+                    final = tuple(insertion)
+                    result.add(final)
+                complements[nums[j]] = j
+        return result
