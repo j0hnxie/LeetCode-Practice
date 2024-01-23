ca8d70106be925c7936fd853f4134b0c7d4e2653
John Xie <johnjxie@gmail.com>
2024-01-23 00:30:52 -0500

    Time: 94 ms (55.65%), Space: 57.7 MB (5.37%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,10 +1,30 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
+        
+#         unique = []
+#         for s in arr:
+#             u = set(s)
+#             if len(u) == len(s): unique.append(u)
+        
+#         # [2] now start with an empty concatenation and iteratively
+#         #    increase its length by trying to add more strings
+#         concat = [set()]
+#         for u in unique:
+#             for c in concat:
+#                 print(type(u))
+#                 print(type(c))
+#                 if not c & u:
+#                     concat.append(c | u)
+#         return max(len(cc) for cc in concat)
+        noRepeats = []
+        for i in arr:
+            if len(i) == len(set(i)):
+                noRepeats.append(i)
+        
         dp = [set()]
-        for a in arr:
-            if len(set(a)) < len(a): continue
-            a = set(a)
-            for c in dp[:]:
-                if a & c: continue
-                dp.append(a | c)
-        return max(len(a) for a in dp)
+        for i in noRepeats:
+            for j in dp:
+                if not set(i) & j:
+                    dp.append(set(i) | j)
+        return max(len(perm) for perm in dp)
+                    
