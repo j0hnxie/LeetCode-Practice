31d638f8f631139d8424d0f26640379851101133
John Xie <johnjxie@gmail.com>
2024-02-11 21:08:51 -0500

    Time: 146 ms (36.02%), Space: 24.5 MB (20.27%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,22 +1,14 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        memo = [[False for i in range(n)] for j in range(n)]
-        count = 0
-        
-        for i in range(n):
-            memo[i][i] = True
-            count += 1
-        
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                memo[i][i + 1] = True
-                count += 1
-        
-        for i in range(3, n + 1):
-            for j in range(n - i + 1):
-                if s[j] == s[j + i - 1] and memo[j + 1][j + i - 2]:
-                    memo[j][j + i - 1] = True
-                    count += 1
-                
+        palindrome = [[False] * n for _ in range(n)]
+        ans = 0
+
+        for length in range(1, n + 1):
+            for i in range(n - length + 1):
+                if s[i] == s[i + length - 1] and (length <= 2 or palindrome[i + 1][i + length - 2]):
+                    palindrome[i][i + length - 1] = True
+                    ans += 1
+
+        return ans
         return count
