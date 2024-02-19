2704ba64f9e15f7c5366dc192b78534ed9640fd5
John Xie <johnjxie@gmail.com>
2024-02-19 18:01:05 -0500

    Time: 51 ms (52.48%), Space: 17.7 MB (33.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,48 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
         
-        
-        sDict = collections.defaultdict(int)
-        
-        for i in s:
-            sDict[i] += 1
-        
-        for i in t:
-            sDict[i] -= 1
-            
-        for keys, values in sDict.items():
-            if sDict[keys] != 0:
-                return False
-            
-        return True
-        
-#         for i in range(128):
-#             sDict[chr(i)] = 0
-#             tDict[chr(i)] = 0
-        
-#         for i in s:
-#             sDict[i] = sDict[i] + 1
-        
-#         for i in t:
-#             tDict[i] = tDict[i] + 1
-            
-#         for key, item in sDict.items():
-#             if item != 0:
-#                 print(key + " " + str(item))
-            
-#         for key, item in tDict.items():
-#             if item != 0:
-#                 print(key + " " + str(item))
-        
-#         for key, item in sDict.items():
-#             # if item != 0 or tDict[key] != 0:
-#                 # print(key + " " + str(item) + " " + str(tDict[key]))
-#             if item != tDict[key]:
-#                 return False
-        
-#         return True
+        return s == t
