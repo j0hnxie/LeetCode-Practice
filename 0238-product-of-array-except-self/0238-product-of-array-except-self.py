b6e5eea28cd809c298087c795639a2d9ea8f5aef
John Xie <johnjxie@gmail.com>
2024-02-19 18:27:34 -0500

    Time: 163 ms (74.96%), Space: 25 MB (25.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,33 +1,25 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        front = [0 for i in range(n)]
+        product = 1
         
-        res = [0] * len(nums)
-        pre = [0] * len(nums)
-        pre[0] = nums[0]
-        post = [0] * len(nums)
-        post[-1] = nums[-1] 
+        for i in range(n):
+            num = nums[i]
+            product *= num
+            front[i] = product
         
-        for i in range(1, len(nums)):
-            pre[i] = pre[i - 1] * nums[i]
+        back = [0 for i in range(n)]
+        product = 1
+        for i in range(n - 1, -1, -1):
+            num = nums[i]
+            product *= num
+            back[i] = product
         
-        for i in range(len(nums) - 2, -1, -1):
-            post[i] = post[i + 1] * nums[i]
+        output = [0 for i in range(n)]
+        for i in range(n):
+            left = front[i - 1] if i > 0 else 1
+            right = back[i + 1] if i < n - 1 else 1
+            output[i] = left * right
+        return output
             
-        # print(pre)
-        # print(post)
-        
-        for i in range(len(nums)):
-            if i == 0:
-                res[i] = post[i + 1]
-            elif i == len(nums) - 1:
-                res[i] = pre[i - 1]
-            else:
-                res[i] = pre[i - 1] * post[i + 1]
-            
-        return res
-            
-                
