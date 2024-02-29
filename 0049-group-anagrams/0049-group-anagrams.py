c9c956264f35bfab10ffbaa2b828ef5efa93a61c
John Xie <johnjxie@gmail.com>
2024-02-29 18:26:44 -0500

    Time: 117 ms (11.66%), Space: 19.3 MB (96.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
+        
         for s in strs:
-            sort = str(sorted(s))
-            if sort in groups:
-                groups[sort].append(s)
+            counts = [0 for i in range(26)]
+            for char in s:
+                counts[ord(char) - 97] += 1
+            
+            code = ""
+            for i in range(26):
+                if counts[i] != 0:
+                    code += chr(i + 97)
+                    code += str(counts[i])
+            if code in groups:
+                groups[code].append(s)
             else:
-                groups[sort] = [s]
+                groups[code] = [s]
         
-        res = []
-        for key, value in groups.items():
-            res.append(value)
-        
+        res = [value for key, value in groups.items()]
         return res
-        
