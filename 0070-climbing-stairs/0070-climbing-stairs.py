89bd6980bd8e25bc3ca1e04d2790fd5e8e198678
John Xie <johnjxie@gmail.com>
2024-03-02 01:23:20 -0500

    Time: 37 ms (43.87%), Space: 16.4 MB (99.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [1, 1]
+        memo = [1 for i in range(n + 1)]
         
         for i in range(2, n + 1):
-            dp.append(dp[i-1] + dp[i-2])
-        
-        return dp[n]
+            memo[i] = memo[i - 1] + memo[i - 2]
+        return memo[n]
