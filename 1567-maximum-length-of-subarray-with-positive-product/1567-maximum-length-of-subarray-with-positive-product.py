2dd7a43396fd4cab476f0fdc9f9778c964669a9a
John Xie <johnjxie@gmail.com>
2024-03-09 15:29:00 -0500

    Time: 4160 ms (5.08%), Space: 30.1 MB (87.96%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -2,21 +2,23 @@ class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
         n = len(nums)
         prefix = 1
-        firstNeg = float('inf')
-        maxLen = -1
+        firstNeg = -1
+        maxLen = 0
         lastZero = -1
         
         for idx, num in enumerate(nums):
             prefix *= num
             if prefix < 0:
-                if firstNeg == float('inf'):
+                if firstNeg == -1:
                     firstNeg = idx
-                curLen = idx - firstNeg
+                    curLen = -1
+                else:
+                    curLen = idx - firstNeg
             elif prefix > 0:
                 curLen = idx - lastZero
             else:
                 lastZero = idx
-                firstNeg = float('inf')
+                firstNeg = -1
                 prefix = 1
                 curLen = 0
             
