6e2b73588295bbb4b7091e9f67ad6914c1775e60
John Xie <johnjxie@gmail.com>
2024-03-09 23:55:22 -0500

    Time: 507 ms (75.41%), Space: 16.5 MB (94.79%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,33 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        memo = [1 for i in range(10)]
+        prev = {
+            0: [4, 6],
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [3, 9, 0],
+            5: [],
+            6: [7, 1, 0],
+            7: [2, 6],
+            8: [1, 3],
+            9: [2, 4]
+        }
+        
+        for col in range(2, n + 1):
+            cur = []
+            for row in range(10):
+                numberOfNumbers = 0
+                for num in prev[row]:
+                    numberOfNumbers += memo[num]
+                    numberOfNumbers %= (10 ** 9 + 7)
+                cur.append(numberOfNumbers)
+            memo = cur
+            
+        res = 0
+        for entry in memo:
+            res += entry
+            res %= (10 ** 9 + 7)
+        
+        return res
+        
