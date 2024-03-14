a28cfdcd2f6e70e518b0f6bd63fc112a10467c7e
John Xie <johnjxie@gmail.com>
2024-03-14 11:43:43 -0400

    Time: 225 ms (45.29%), Space: 17.1 MB (99.51%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,17 +1,27 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        prefix = 0
-        prefixs = {}
-        res = 0
+        start = 0
+        prefix_zeros = 0
+        current_sum = 0
+        total_count = 0
         
-        for num in nums:
-            prefix += num
-            if prefix == goal:
-                res += 1
+        # Loop through the array using end pointer
+        for end, num in enumerate(nums):
+            # Add current element to the sum
+            current_sum += num
             
-            comp = prefix - goal
-            res += prefixs.get(comp, 0)
-            prefixs[prefix] = prefixs.get(prefix, 0) + 1
-            # print(res)
-            # print(prefixs)
-        return res
+            # Slide the window while condition is met
+            while start < end and (nums[start] == 0 or current_sum > goal):
+                if nums[start] == 1:
+                    prefix_zeros = 0
+                else:
+                    prefix_zeros += 1
+                
+                current_sum -= nums[start]
+                start += 1
+                
+            # Count subarrays when window sum matches the goal
+            if current_sum == goal:
+                total_count += 1 + prefix_zeros  
+                
+        return total_count
