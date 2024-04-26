63052ec12ad6db865dd52213f56ae6a72fe7e515
John Xie <johnjxie@gmail.com>
2024-04-26 18:13:05 -0400

    Time: 199 ms (100.00%), Space: 20.2 MB (20.70%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -21,8 +21,6 @@ class Solution:
         if n == 1:
             return grid[0][0]
         
-        prev = grid[0]
-        
         min_val = float('inf')
         min_col = -1
         second_min_val = float('inf')
@@ -39,19 +37,17 @@ class Solution:
                 second_min_col = idx
         
         for row in range(1, n):
-            cur = [0] * n
             t_second_min_val = float('inf')
             t_second_min_col = -1
             t_min_val = float('inf')
             t_min_col = -1
             for idx in range(n):
+                val = grid[row][idx]
                 if idx == min_col:
-                    cur[idx] += second_min_val
+                    val += second_min_val
                 else:
-                    cur[idx] += min_val
-                cur[idx] += grid[row][idx]
+                    val += min_val
                 
-                val = cur[idx]
                 if val <= t_min_val:
                     # print(f"{val} less than {t_min_val} and {t_second_min_val}")
                     t_second_min_val = t_min_val
@@ -62,7 +58,6 @@ class Solution:
                     # print(f"{val} only less than {t_min_val} and {t_second_min_val}")
                     t_second_min_val = val
                     t_second_min_col = idx
-            prev = cur
             min_val = t_min_val
             min_col = t_min_col
             second_min_val = t_second_min_val
