682b09ae66312d7dbc74946a20d749da32182b42
John Xie <johnjxie@gmail.com>
2024-04-26 18:10:36 -0400

    Time: 4190 ms (22.03%), Space: 20.4 MB (18.50%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        
         n = len(grid)
         prev = grid[0]
         
@@ -13,5 +14,62 @@ class Solution:
                     cur[col] = min(cur[col], prev[prev_col])
                 cur[col] += grid[row][col]
             prev = cur
-            # print(prev)
-        return min(prev)
+            print(prev)
+        # return min(prev)
+        n = len(grid)
+        
+        if n == 1:
+            return grid[0][0]
+        
+        prev = grid[0]
+        
+        min_val = float('inf')
+        min_col = -1
+        second_min_val = float('inf')
+        second_min_col = -1
+        
+        for idx, val in enumerate(grid[0]):
+            if val <= min_val:
+                second_min_val = min_val
+                second_min_col = min_col
+                min_val = val
+                min_col = idx
+            elif val <= second_min_val:
+                second_min_val = val
+                second_min_col = idx
+        
+        for row in range(1, n):
+            cur = [0] * n
+            t_second_min_val = float('inf')
+            t_second_min_col = -1
+            t_min_val = float('inf')
+            t_min_col = -1
+            for idx in range(n):
+                if idx == min_col:
+                    cur[idx] += second_min_val
+                else:
+                    cur[idx] += min_val
+                cur[idx] += grid[row][idx]
+                
+                val = cur[idx]
+                if val <= t_min_val:
+                    print(f"{val} less than {t_min_val} and {t_second_min_val}")
+                    t_second_min_val = t_min_val
+                    t_second_min_col = t_min_col
+                    t_min_val = val
+                    t_min_col = idx
+                elif val <= t_second_min_val:
+                    print(f"{val} only less than {t_min_val} and {t_second_min_val}")
+                    t_second_min_val = val
+                    t_second_min_col = idx
+            prev = cur
+            min_val = t_min_val
+            min_col = t_min_col
+            second_min_val = t_second_min_val
+            second_min_col = t_second_min_col
+            print(prev)
+            print(t_min_val)
+            print(t_min_col)
+            print(t_second_min_val)
+            print(t_second_min_col)
+        return min_val
