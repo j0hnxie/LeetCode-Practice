5af2fc9f3ac09507f860ac5dc99225a163544727
John Xie <johnjxie@gmail.com>
2024-06-06 19:05:46 -0700

    Time: 41 ms (84.45%), Space: 16.5 MB (89.17%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,31 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        values = {
-            1: "I",
-            5: "V",
-            10: "X",
-            50: "L",
-            100: "C",
-            500: "D",
-            1000: "M",
-        }
-        multiplier = 1
-        cur = num
-        res = ""
-        while multiplier <= num:
-            # print(cur)
-            cur_dig = cur % 10
-            cur_val = cur_dig * multiplier
-            if cur_dig == 9:
-                res = values[multiplier] + values[cur_val + multiplier] + res
-            elif cur_dig >= 5:
-                res = values[5 * multiplier] + (cur_dig - 5) * values[multiplier] + res
-            elif cur_dig == 4:
-                res = values[multiplier] + values[5 * multiplier] + res
-            else:
-                res = cur_dig * values[multiplier] + res
-            cur = int(cur / 10)
-            multiplier *= 10
-        return res
-        
-        
+
+        m = ["", "M", "MM", "MMM"]
+        c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return m[num//1000] + c[(num%1000)//100] + x[(num%100)//10] + i[num%10]
