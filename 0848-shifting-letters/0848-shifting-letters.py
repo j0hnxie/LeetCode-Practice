0e20500c866f2a75baa99982b49328788b38d74d
John Xie <johnjxie@gmail.com>
2024-06-13 17:54:37 -0700

    Time: 573 ms (60.86%), Space: 29.9 MB (71.95%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -6,9 +6,9 @@ class Solution:
         
         for idx in range(n - 1, -1, -1):
             total += shifts[idx]
-            char_val = ord(s[idx]) - 97
+            char_val = ord(s[idx]) - ord('a')
             new_val = (char_val + total) % 26
-            new_char = chr(new_val + 97)
+            new_char = chr(new_val + ord('a'))
             res += new_char
         
         res = res[::-1]
