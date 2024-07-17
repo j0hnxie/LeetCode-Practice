2f558701e7da8b5b7d0c1046d7b29f09ec6cf12d
John Xie <johnjxie@gmail.com>
2024-07-16 18:10:21 -0700

    Time: 73 ms (18.57%), Space: 17 MB (12.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         stack = deque()
