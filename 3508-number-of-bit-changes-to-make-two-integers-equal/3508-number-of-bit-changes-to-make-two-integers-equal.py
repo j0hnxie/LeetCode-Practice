f9ea48182ac7d6c700f828361c2e67f3c03309ec
John Xie <johnjxie@gmail.com>
2024-07-20 21:23:23 -0700

    Time: 36 ms (50%), Space: 16.6 MB (100%) - LeetHub

--- 3508-number-of-bit-changes-to-make-two-integers-equal/3508-number-of-bit-changes-to-make-two-integers-equal.py
+++ 3508-number-of-bit-changes-to-make-two-integers-equal/3508-number-of-bit-changes-to-make-two-integers-equal.py
@@ -1,22 +1,6 @@
 class Solution:
     def minChanges(self, n: int, k: int) -> int:
-        n = str(bin(n))[2:]
-        k = str(bin(k))[2:]
-
-        if len(n) < len(k):
-            return -1
-        
-        res = 0
-        for idx in range(len(k)):
-            kIdx = len(k) - 1 - idx
-            nIdx = len(n) - 1 - idx
-            if n[nIdx] == "1" and k[kIdx] == "0":
-                res += 1
-            elif n[nIdx] == "0" and k[kIdx] == "1":
-                return -1
-        remaining = len(n) - len(k)
-        for idx in range(remaining):
-            nIdx = len(n) - len(k) - 1 - idx
-            if n[nIdx] == "1":
-                res += 1
-        return res
+        k ^= n
+        diffs = bin(k).count("1")
+        k &= n
+        return diffs if diffs == bin(k).count("1") else -1
