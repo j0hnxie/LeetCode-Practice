bfb71cf3ae50b4024b7e0b31ec3995af1419b128
John Xie <johnjxie@gmail.com>
2024-08-18 19:53:14 -0400

    Time: 88 ms (71.52%), Space: 16.2 MB (100%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,29 +1,24 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        generators = [2, 3, 5]
-        factors = [deque([2]), deque([3]), deque([5])]
-        visited = set([1, 2, 3, 5])
+        uglies = [0] * n
+        uglies[0] = 1
+        two_idx, three_idx, five_idx = 0, 0, 0
+        next_two, next_three, next_five = 2, 3, 5
 
-        res = [1]
-        pq = [(2, 0), (3, 1), (5, 2)]
-        heapq.heapify(pq)
-        while len(res) < n:
-            value, idx = heapq.heappop(pq)
-            res.append(value)
+        for idx in range(1, n):
+            cur_min = min([next_two, next_three, next_five])
+            uglies[idx] = cur_min
 
-            if value * 2 not in visited:
-                factors[0].append(value * 2)
-                visited.add(value * 2)
+            if cur_min == next_two:
+                two_idx += 1
+                next_two = 2 * uglies[two_idx]
 
-            if value * 3 not in visited:
-                factors[1].append(value * 3)
-                visited.add(value * 3)
+            if cur_min == next_three:
+                three_idx += 1
+                next_three = 3 * uglies[three_idx]
 
-            if value * 5 not in visited:
-                factors[2].append(value * 5)
-                visited.add(value * 5)
-
-            factors[idx].popleft()
-            heapq.heappush(pq, (factors[idx][0], idx))
-
-        return res[-1]
+            if cur_min == next_five:
+                five_idx += 1
+                next_five = 5 * uglies[five_idx]
+        
+        return uglies[-1]
