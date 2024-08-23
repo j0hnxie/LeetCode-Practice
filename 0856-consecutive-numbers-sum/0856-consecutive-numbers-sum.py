71bcd3926c59982a21fdaab75a2fcb5ef5faef59
John Xie <johnjxie@gmail.com>
2024-08-22 22:15:56 -0400

    Time: 128 ms (34.82%), Space: 16.5 MB (41.41%) - LeetHub

---
+++ 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def consecutiveNumbersSum(self, n: int) -> int:
+        result = 0
+
+        k = 1
+        while (k * (k - 1)) // 2 < n:
+            if (n - ((k * (k - 1)) // 2)) % k == 0:
+                result += 1
+            k += 1
+        return result
