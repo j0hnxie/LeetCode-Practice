23d61c5fd11a01db2deab9561491f992074186e4
John Xie <johnjxie@gmail.com>
2024-10-17 16:57:04 -0400

    Time: 7 ms (100%), Space: 18.9 MB (5.97%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,28 +1,29 @@
-# class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         adj_list = defaultdict(list)
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj_list = defaultdict(list)
 
-#         for frm, to, price in flights:
-#             adj_list[frm].append((to, price))
+        for frm, to, price in flights:
+            adj_list[frm].append((to, price))
 
-#         pq = [(0, 0, src)]
-#         dist = [float('inf')] * n
-#         dist[src] = 0
+        pq = [(0, 0, src)]
+        seen = [float('inf')] * n
+        dist[src] = 0
 
-#         while pq:
-#             dist_so_far, stops, node  = heapq.heappop(pq)
+        while pq:
+            dist_so_far, stops, node  = heapq.heappop(pq)
 
-#             if node == dst:
-#                 return dist_so_far
+            if node == dst:
+                return dist_so_far
             
-#             if stops > k:
-#                 continue
-            
-#             for nex, price in adj_list[node]:
-#                 dist[nex] = min(dist[nex], dist_so_far + price)
-#                 heapq.heappush(pq, (dist_so_far + price, stops + 1, nex))
+            if stops > k or stops > seen[node]:
+                continue
 
-#         return dist[dst] if dist[dst] != float('inf') else -1
+            seen[node] = stops
+            
+            for nex, price in adj_list[node]:
+                heapq.heappush(pq, (dist_so_far + price, stops + 1, nex))
+
+        return -1
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
