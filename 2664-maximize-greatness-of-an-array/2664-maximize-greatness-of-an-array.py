c9e83525177762b998ee3c6b5058604e5b0fb8c9
John Xie <johnjxie@gmail.com>
2024-11-14 17:06:18 -0500

    Time: 24 ms (86.59%), Space: 30.5 MB (86.92%) - LeetHub

--- 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.py
+++ 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.py
@@ -1,19 +1,11 @@
 class Solution:
     def maximizeGreatness(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort(reverse=True)
-        # lower, upper = sorted(nums), sorted(nums)
+        nums.sort()
 
-        upper_l, upper_r = 0, n - 1
-        lower_l = 0
         result = 0
-        while upper_l <= upper_r:
-            upper_value, lower_value = nums[upper_l], nums[lower_l]
-            if upper_value <= lower_value:
-                upper_r -= 1
-            else:
+        for num in nums:
+            if num > nums[result]:
                 result += 1
-                upper_l += 1
-            lower_l += 1
-
+                
         return result
