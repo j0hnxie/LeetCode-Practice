8783c4ab90adad7aea912d79104b6f70bf140440
John Xie <johnjxie@gmail.com>
2025-04-02 03:50:02 -0400

    Time: 0 ms (100%), Space: 18 MB (8.98%) - LeetHub

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+
+        result = 0
+        largest = max(nums[1], nums[0])
+        gap = nums[0] - nums[1]
+
+        for k in range(2, n):
+            print(largest, gap, result)
+            result = max(result, gap * nums[k])
+            gap = max(gap, largest - nums[k])
+            largest = max(largest, nums[k])
+        
+        return result
